beaf50ff054454d479172f07ee0a6f4afd51d112
anupam <anupamjain177@gmail.com>
2024-06-10 15:50:04 -0400

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Login from "./Login";
 
 const Navbar = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme")?localStorage.getItem("theme"):"light")
@@ -139,9 +140,10 @@ const Navbar = () => {
               </svg>
             </label>
             <div className="">
-              <a className="bg-black px-3 py-2 text-white p-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer">
+              <a className="bg-black px-3 py-2 text-white p-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer " onClick={()=> document.getElementById("my_modal_3").showModal()}>
                 Login
               </a>
+              <Login/>
             </div>
           </div>
         </div>
