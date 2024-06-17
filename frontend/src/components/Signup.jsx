7ffd8528d14665f9ba60aefa014a93af21a3630d
AnupamJainZap <166516010+AnupamJainZap@users.noreply.github.com>
2024-06-17 15:53:03 -0400

    updated

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Login from "./Login";
 import { useForm } from "react-hook-form";
-
+import axios from "axios";
 const Signup = () => {
   const {
     register,
@@ -10,7 +10,24 @@ const Signup = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  
+
+  const onSubmit = async (data) => {
+    const userInfo={
+      fullname: data.fullname,
+      email: data.email,
+      password: data.password,
+    };
+    await axios.post("http://localhost:4001/user/signup", userInfo)
+    .then((res)=>{
+      console.log(res.data);
+        if (res.data) {
+        alert("Signup Successfully")
+      }
+    }).catch((err)=>{
+      alert("Error: "+ err)
+    })
+  }
   return (
     <>
       <div className="flex h-screen items-center justify-center ">
